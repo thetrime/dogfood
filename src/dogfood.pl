342bb531893c381a08018689e68584d3586a0447
Matt Lilley <thetrime@gmail.com>
2017-02-26 12:06:33 +0000

    Steady progress

--- src/dogfood.pl
+++ src/dogfood.pl
@@ -4,23 +4,33 @@ render(_State, _Props, Form):-
         {|jsx(Form)||
         <Panel fill="both">
           <Label label="Welcome to Dogfood"/>
+          <Field fill="horizontal"/>
           <Panel fill="both" scroll="vertical">
             {Rows}
           </Panel>
         </Panel>|},
-        findall(Row,
-                ( data(Label),
-                  {|jsx(Row)||
-                  <Panel layout="horizontal" fill="horizontal">
-                    <Label label={Label} fill="horizontal"/>
-                    <Button image="/assets/tick32.png" fill="none" onClick={this.obtained(Label)}/>
-                  </Panel>|}
-                ),
-                Rows).
+        findall(Data,
+                data(Data),
+                Items),
+        make_rows(Items, 0, Rows).
+                
+make_rows([], _, []):- !.
+make_rows([Item|Items], N, [Row|Rows]):-
+        NN is N+1,
+        ( N mod 2 =:= 1->
+            Zebra = ''
+        ; Zebra = 'lightgrey'
+        ),
+        {|jsx(Row)||
+        <Panel layout="horizontal" fill="horizontal" background={Zebra}>
+          <Label label={Item} fill="horizontal"/>
+          <Button image="/assets/tick32.png" fill="none" onClick={this.obtained(Item)}/>
+        </Panel>|},
+        make_rows(Items, NN, Rows).
 
 data('Banjo').
 data('Mango').
 data('Dingo').
 data(Label):-
-        between(0, 60, X),
+        between(0, 20, X),
         format(atom(Label), 'Item ~w', [X]).
